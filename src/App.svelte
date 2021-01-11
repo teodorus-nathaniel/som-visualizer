27a3f5766d1a13f6877f490232296b101bca8397
frandirianto <frandirianto@gmail.com>
2021-01-11 20:14:56 +0700

    added zoombar

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,42 @@
 <script lang="ts">
-  import CartesianGraph from './components/CartesianGraph.svelte';
-  import Tailwind from './Tailwind.svelte';
+    import { onDestroy, onMount } from "svelte/internal";
 
-  let data = [
-    {
-      x: 5,
-      y: 5,
-    },
-    {
-      x: 20,
-      y: 20,
-    },
-  ];
+    import CartesianGraph from "./components/CartesianGraph.svelte";
+    import ZoomBar from "./components/ZoomBar.svelte";
+    import Tailwind from "./Tailwind.svelte";
+
+    let data = [
+        {
+            x: 5,
+            y: 5,
+        },
+        {
+            x: 20,
+            y: 20,
+        },
+    ];
+
+    let scale = 10;
+
+    const handleZoom = (zoom: CustomEvent<number>) => {
+        scale = zoom.detail;
+    };
+
+    function onMouseWheel(e) {
+        console.log(scale);
+        if (e.deltaY > 0) scale = Math.max(1, scale - 1);
+        else scale = Math.min(20, scale + 1);
+    }
+
+    onMount(() => {
+        window.addEventListener("mousewheel", onMouseWheel);
+    });
+
+    onDestroy(() => {
+        window.removeEventListener("mousewheel", onMouseWheel);
+    });
+
+    $: console.log(scale);
 </script>
 
 <style type="text/scss">
@@ -19,5 +44,6 @@
 
 <Tailwind />
 <main>
-  <CartesianGraph points={data} scale={10} />
+    <ZoomBar on:zoom={handleZoom} {scale} />
+    <CartesianGraph points={data} {scale} />
 </main>
