7e31c8158a73921e88cb024e8622c63a9ee543f5
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-06-20 23:10:34 +0700

    Fix global styles not applying

--- src/App.svelte
+++ src/App.svelte
@@ -15,27 +15,6 @@
   } | null = null;
 </script>
 
-<style type="text/scss">
-  :global(input[type="range"]) {
-    background-color: #fff;
-    overflow: hidden;
-  }
-  :global(.floating-container) {
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 8px;
-    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.1);
-  }
-
-  :global(*) {
-    font-family: Montserrat, sans-serif;
-  }
-
-  :global(:root) {
-    --primary: #325eb2;
-  }
-</style>
-
 <Tailwind />
 <main>
   <CustomizeModel
--- src/Tailwind.svelte
+++ src/Tailwind.svelte
@@ -2,4 +2,23 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  input[type="range"] {
+    background-color: #fff;
+    overflow: hidden;
+  }
+  .floating-container {
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.1);
+  }
+
+  * {
+    font-family: Montserrat, sans-serif;
+  }
+
+  :root {
+    --primary: #325eb2;
+  }
 </style>
